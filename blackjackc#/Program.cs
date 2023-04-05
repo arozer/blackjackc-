3cfde7baaf126a00a1ce8be013b77a79f5407d8a
arozer <georginikolajev27s@gmail.com>
2023-04-05 22:30:55 +0300

    realease v.1

--- blackjackc#/Program.cs
+++ blackjackc#/Program.cs
@@ -1,35 +1,31 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
-string choose = "n";
+startgame();
+void startgame(){
+    string start = "n";
 
-while (choose != "y") // начало игры,играть или не играть вот в чём вопрос
-{ 
-    Console.WriteLine("Welcome to fuck your moth... a mean blackjack!");
-    Console.WriteLine("to start game write: 'y'");
-    choose = Console.ReadLine();
-    if(choose == "y")
+    while (start != "y") // начало игры,играть или не играть вот в чём вопрос
     {
-        startgame(); // Запуск игры
+        Console.WriteLine("Welcome to fuck your moth... a mean blackjack!");
+        Console.WriteLine("to start game write: 'y'");
+        start = Console.ReadLine();
+        if (start == "y")
+        {
+            game(); // Запуск игры
+        }
     }
 }
 
 
-void startgame() // начало игры и его геймплей
+void game() // начало игры и его геймплей
 {
     int number = 0;
     Console.WriteLine("handing out cards....// ");
 
     Players player = new Players("aroz", 0, ""); // создаётся игрок
     Players dealer = new Players("dealer", 0, "");// создаётся дилер
-
-    void GiveRandomCard (Players player)
-    {
-        // переменная для вывода карты и счёта
-        player.value += shuffle(out number); // прибавляем значение карты к счёту
-        Deck card_dealer = (Deck)number; // вытаскиваем карту из перечисления
-        player.cards += card_dealer + "/"; // объединение новых карт, для вывода руки
-                                           
-    }
 
     for (int i = 0; i < 2; i++) // раздача стартовых карт игрокам
     {
@@ -46,6 +42,30 @@ void startgame() // начало игры и его геймплей
     }
 
     gameplay();
+
+    void GiveRandomCard(Players player)
+    {
+        // переменная для вывода карты и счёта
+        player.value += shuffle(out number); // прибавляем значение карты к счёту
+        Deck card_dealer = (Deck)number; // вытаскиваем карту из перечисления
+        player.cards += card_dealer + "/"; // объединение новых карт, для вывода руки
+        checkAce();
+        void checkAce()
+        {
+            if (player.cards.Contains("A") && player.value <= 10)
+            {
+                player.value += 10;
+            }
+
+            if (dealer.cards.Contains("A") && dealer.value <= 10)
+            {
+                dealer.value += 10;
+            }
+        }
+
+    }
+
+
     static int shuffle(out int number) // генерация случайной карты
     {
         Random rnd = new Random();
@@ -53,26 +73,129 @@ void startgame() // начало игры и его геймплей
         return number;
 
     }
+
+
     void gameplay()
     {
-        if (player.cards.Contains("A") && player.value <= 10)
-        {
-                player.value += 10;
-        }
+        gameActions();
 
-        if (dealer.cards.Contains("A") && dealer.value <= 10)
+        void gameActions()
         {
-                dealer.value += 10;
-        }
+            string choose = "y";
+            score(player,dealer);
+            while (choose == "y") // пока игрок не пасанёт,цикл будет предлогать добор
+            {
+                Console.WriteLine("хотите взять карту или спасовать? 'y' - взять, 'n' - пас ");
+                choose = Console.ReadLine();
+                if (choose == "y")
+                {
+                    GiveRandomCard(player); // игрок добирает карты
+                    score(player, dealer);
+                }
+                else if (dealer.value >= 17) // при счёте dealer 17 и более не идёт добор
+                {
+                    score(player, dealer);
+                }
+            }
 
-        Console.WriteLine($"Рука игрока {player.name}: /{player.cards} (счёт:{player.value})");
-        Console.WriteLine($"Рука игрока {dealer.name}: /{dealer.cards} (счёт:{dealer.value})");
+            while (choose == "n") // дилер добирает нужное кол-во пи пасе игрока
+            {
+                if (dealer.value >= 17 && dealer.value <= 22) // при счёте dealer 17 и более не идёт добор
+                {
+                    score(player, dealer);
+                    choose = "a";
+                }
+                else
+                {
+                    GiveRandomCard(dealer);
+                    score(player, dealer);
+                }
+            }
+            void score(Players player, Players dealer)
+            {
+                player.value = 20;
+                dealer.value = player.value;
+                Console.WriteLine($"Рука игрока {player.name}: /{player.cards} (счёт:{player.value})");
+                Console.WriteLine($"Рука игрока {dealer.name}: /{dealer.cards} (счёт:{dealer.value})");
+                switch (player.value)
+                {
+                    case 21:
+                        endgame(win.playerWin);
+                        startgame();
+                        break;
+                    case > 21:
+                        endgame(win.playerLose);
+                        startgame();
+                        break;
+                    case < 21:
+                        endgame(win.playerInGame);
+                        break;
+                }
+                switch (dealer.value)
+                {
+                    case 21:
+                        endgame(win.playerLose);
+                        startgame();
+                        break;
+                    case > 21:
+                        endgame(win.playerWin);
+                        startgame();
+                        break;
+                    case < 21:
+                        endgame(win.playerInGame);
+                        break;
+                }
+                if (dealer.value == player.value  && dealer.value >= 17 && choose == "n")
+                {
+                    endgame(win.tie);
+                    startgame();
+                }
+                if (dealer.value >= 17 && dealer.value > player.value && choose == "n")
+                {
+                    endgame(win.playerLose);
+                    startgame();
+                }
+                else if(dealer.value >= 17 && dealer.value < player.value && choose == "n")
+                {
+                    endgame(win.playerWin);
+                    startgame();
+                }
+
+                void endgame(win status)
+                {
+                    switch (status)
+                    {
+                        case win.playerWin:
+                            Console.WriteLine("вы выйграли!");
+                            break;
+                        case win.playerLose:
+                            Console.WriteLine("вы Проебали!");
+                            break;
+                        case win.tie:
+                            Console.WriteLine("ничья");
+                            break;
+                    }
+                }
+
+            }
+
+        }
 
     }
-}
+
+
+
+
+
+
+
+    }
+
+
         
 enum Deck {A = 1,two = 2, three = 3, four = 4, five = 5, // перечесление колоды карт
     six = 6, seven = 7, eight = 8, nine = 9, ten = 10, J = 10, Q = 10,K = 10,};
+enum win { playerWin = 1, playerInGame = 2, playerLose = 3, tie = 4 }
 class Players // класс для создание игрока и дилера
 {
     public string name;
